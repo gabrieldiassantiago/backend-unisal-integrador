dae91f20c89f018a1b61f2b04906f9eaf67aa559
Gabriel Dias Santiago <gabrieldiassantiago@hotmail.com>
2025-07-31 20:39:28 -0300

    feat: adiciona estrutura inicial de autenticação

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post, Body, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dtos/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    const user = await this.authService.validateUser(loginDto.email, loginDto.password);
+    if (!user) {
+      throw new UnauthorizedException('E-mail ou senha inválidos.');
+    }
+    return this.authService.login(user);
+  }
+}
