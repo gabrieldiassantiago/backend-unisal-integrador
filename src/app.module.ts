43b55a9e870c5842a9fc0ce1566ad4f5e55a39cf
Gabriel Dias Santiago <gabrieldiassantiago@hotmail.com>
2025-07-31 20:38:05 -0300

    chore: atualiza AppModule com novos módulos ou dependências

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { WhatsappController } from './whatsapp/whatsapp.controller';
 import { DevicesModule } from './devices/devices.module';
 import { FallAlertsModule } from './fall-alerts/fall-alerts.module';
 import { EmergencyContactsModule } from './emergency-contacts/emergency-contacts.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, WhatsappModule, DevicesModule, FallAlertsModule, EmergencyContactsModule],
+  imports: [UsersModule, DevicesModule, AuthModule, FallAlertsModule, EmergencyContactsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
